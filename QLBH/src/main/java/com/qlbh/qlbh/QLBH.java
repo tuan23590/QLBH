f91ff7a3c2f9d51e4b49df3cd4d4885a37f7aab7
tuan23590 <thanhtuan.htcv@gmail.com>
2023-10-11 18:18:44 +0700

    test

---
+++ QLBH/src/main/java/com/qlbh/qlbh/QLBH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.qlbh.qlbh;
+
+/**
+ *
+ * @author Tuan
+ */
+public class QLBH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
